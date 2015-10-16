2b8eae21e2d27033d2cfc879e823609b3a10e0a9
Filip Wasil <fillwave@gmail.com>
2015-10-16 21:25:32 +0200

    Important fix regarding buffers clean up.

--- src/core/buffers/Buffer.cpp
+++ src/core/buffers/Buffer.cpp
@@ -5,30 +5,6 @@
  * Author: Filip Wasil
  */
 
-/*************************************************************************
- *
- * Copyright (C) 2014 Filip Wasil
- *
- * Filip Wasil CONFIDENTIAL
- * __________________
- *
- *  [2012] - [2014] Filip Wasil
- *  All Rights Reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of Filip Wasil. The intellectual and technical
- * concepts contained herein are proprietary to Filip Wasil
- * and his suppliers and may be covered by Polish and Foreign
- * Patents, patents in process, and are protected by trade secret
- * or copyright law. Dissemination of this information or reproduction
- * of this material is strictly forbidden unless prior written
- * permission is obtained from Filip Wasil.
- *
- * To use the code, you must contact the author directly and ask permission.
- *
- * filip.wasil@gmail.com
- *
- */
 
 #include <fillwave/extras/Log.h>
 #include <fillwave/core/buffers/Buffer.h>
@@ -49,7 +25,7 @@ Buffer::Buffer(GLuint target, GLuint drawType, GLuint index, GLsizei howMany)
 }
 
 Buffer::~Buffer() {
-	glGenBuffers(mHowMany, mHandles);
+	glDeleteBuffers(mHowMany, mHandles);
 }
 
 void Buffer::setElements(GLuint elements) {
