2141aaf18de0a461629961785de7c45b95ff3828
Filip Wasil <filipwasil@Mac-mini-Filip.local>
2015-11-08 00:26:33 +0100

    Build system configured against iOS, but iOS still in progress

--- scripts/build_ios.sh
+++ scripts/build_ios.sh
@@ -7,17 +7,17 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 brew uninstall libfillwave libfillwave-dev
 
 # Prepare build directories
-mkdir ../../fillwave_osx_build
-rm -rf ../../fillwave_osx_build/*
+mkdir ../../fillwave_ios_build
+rm -rf ../../fillwave_ios_build/*
 
 # Enter build directory
-cd ../../fillwave_osx_build
+cd ../../fillwave_ios_build
 
 # Install other stuff
 brew install cmake gcc g++ glm freetype
 
 # we are fine now. go.
-cmake ../fillwave -G"Xcode" -DNDEBUG=OFF -DBUILD_PACK=ON -DBUILD_LIB=ON -DBUILD_DEV=ON -DVERSION_MAJOR=1 -DVERSION_MINOR=0 -DVERSION_PATCH=0 -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DCMAKE_TOOLCHAIN_FILE=../fillwave/ext/ios-cmake/toolchain/iOS.cmake && cpack
+cmake ../fillwave -G"Xcode" -DNDEBUG=OFF -DFILLWAVE_BUILD_PACK=ON -DFILLWAVE_BUILD_LIB=ON -DFILLWAVE_BUILD_DEV=OFF -DVERSION_MAJOR=1 -DVERSION_MINOR=0 -DVERSION_PATCH=0 -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DCMAKE_TOOLCHAIN_FILE=../fillwave/ext/ios-cmake/toolchain/iOS.cmake
 
 # Crate TGZ package
 cpack
