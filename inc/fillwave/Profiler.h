fd21e9ded411951cf2d8d7658fb550035f59bc0e
Filip Wasil <fillwave@gmail.com>
2016-04-12 21:08:06 +0200

    Profiling macros introduced

--- inc/fillwave/Profiler.h
+++ inc/fillwave/Profiler.h
@@ -15,11 +15,32 @@
 
 #include <chrono>
 #include <thread>
+#include <iostream>
 
 #define STR_HELPER(x) #x
 #define STR(x) STR_HELPER(x)
 
 #define FILLWAVE_AO_SAMPLE_RADIUS 1.6f
 #define FILLWAVE_OQ_VERTICES 36
+
+#define FILLWAVE_MEASURE(info, foo)  do {  \
+    std::chrono::time_point<std::chrono::system_clock> start, end; \
+    start = std::chrono::system_clock::now(); \
+    foo; \
+    end = std::chrono::system_clock::now();\
+    std::chrono::duration<double> elapsed_seconds = end-start;\
+    std::cout << info << elapsed_seconds.count() << std::endl;} while(0);\
+
+#define FILLWAVE_MEASURE_START std::chrono::system_clock::now
+#define FILLWAVE_MEASURE_STOP(info, start)  do {  \
+    std::chrono::duration<double> elapsed_seconds = std::chrono::system_clock::now() - start;\
+    std::cout << info << elapsed_seconds.count() << std::endl;} while(0);
+
+/* Usage
+ *  auto start = FILLWAVE_MEASURE_START();
+ *  jobs ...
+ *  FILLWAVE_MEASURE_STOP("The period passed:", start)
+ * */
+
 
 #endif /* SRC_PROFILER_H_ */
