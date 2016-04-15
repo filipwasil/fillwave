a746c49ca084ead188bf30e31e3df219f3594837
Filip Wasil <fillwave@gmail.com>
2016-04-15 06:50:59 +0200

    Logging mechanism corrected

--- inc/fillwave/Log.h
+++ inc/fillwave/Log.h
@@ -78,20 +78,20 @@ _AIX            Defined on AIX
 
 /* Define this macro in your source file to provide access to Crater log macros */
 
-#define FLOGINIT_DEFAULT()\
-   static const std::string _tag_ = fillwave::getFileNameOnly(__FILE__);     \
+#define FLOGINIT_DEFAULT()                                              \
+   static const std::string _tag_ = fillwave::getFileNameOnly(__FILE__);\
    static const char _mask_ = FBIT_MAX;
 
-#define FLOGINIT_MASK(mask)\
-   static const std::string _tag_ = (__FILE__);     \
+#define FLOGINIT_MASK(mask)                                             \
+   static const std::string _tag_ = (__FILE__);                         \
    static const char _mask_ = (mask);
 
-#define FLOGINIT(tag, mask)\
-   static const std::string _tag_ = (tag);     \
+#define FLOGINIT(tag, mask)                                             \
+   static const std::string _tag_ = (tag);                              \
    static const char _mask_ = (mask);
 
-#define FLOGINIT_NONE()\
-   static const std::string _tag_ = "";     \
+#define FLOGINIT_NONE()                                                 \
+   static const std::string _tag_ = "";                                 \
    static const char _mask_ = 0;
 
 #define FIF(mask) (::_mask_ & mask)
@@ -118,48 +118,49 @@ _AIX            Defined on AIX
 
 #else /* __ANDROID__ */
 
-#define FLOG_BASE(color, type, flag, ...)																		   \
-	do{																												\
-      if ( FIF(flag) ) {																						\
-			FILE *unique_niosfoinfsd;																			\
-			if (fillwave::isFileValid()) {																   \
-				unique_niosfoinfsd = fopen( fillwave::getLogPath().c_str(), "a" );			   \
-				if (!unique_niosfoinfsd) {                                   						\
-					break;																							\
-				}																										\
-			} else {																									\
-				unique_niosfoinfsd = stdout;																	\
-			}																											\
-			fprintf(unique_niosfoinfsd,"%s%s[%s] <%s:%d> %s", FOPENCOLOROUTPUT, color, FTO_STRING(type),::_tag_.c_str(),__LINE__,FCLOSECOLOROUTPUT); 	\
-			(void)fprintf(unique_niosfoinfsd,__VA_ARGS__);												\
-			fprintf(unique_niosfoinfsd,"\n");																\
-			if (fillwave::isFileValid()) {																   \
-				fclose(unique_niosfoinfsd);																	\
-			}																											\
-      }																												\
+#define FLOG_BASE(color, type, flag, ...)                                          \
+	do {                                                                            \
+	   if ( FIF(flag) ) {                                                           \
+	      FILE *unique_niosfoinfsd;                                                 \
+	      if (fillwave::isFileValid()) {                                            \
+	         unique_niosfoinfsd = fopen( fillwave::getLogPath().c_str(), "a" );     \
+	         if (!unique_niosfoinfsd) {                                             \
+	            break;                                                              \
+	         }                                                                      \
+	      } else {                                                                  \
+	         unique_niosfoinfsd = stdout;                                           \
+	      }                                                                         \
+	      fprintf(unique_niosfoinfsd,"%s%s[%s] <%s:%d> %s", FOPENCOLOROUTPUT, color,\
+	            FTO_STRING(type),::_tag_.c_str(),__LINE__,FCLOSECOLOROUTPUT);       \
+	      (void)fprintf(unique_niosfoinfsd,__VA_ARGS__);                            \
+	      fprintf(unique_niosfoinfsd,"\n");                                       \
+	      if (fillwave::isFileValid()) {                                            \
+	         fclose(unique_niosfoinfsd);                                            \
+	      }                                                                         \
+	   }                                                                            \
 	} while(0)
 
 #define FLOG_CHECK(...)\
-      do { GLenum error_unique_niosfoinfsd = glGetError();                                               \
-         if ( error_unique_niosfoinfsd != GL_NO_ERROR) {                                                 \
-            fprintf(stdout,"[%s 0x%04x] ","CORE ERROR:",error_unique_niosfoinfsd);                       \
-            if (error_unique_niosfoinfsd == 0x0506) { /*Framebuffer error*/                              \
-               GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);              \
-               fprintf(stdout,"[%s 0x%04x] ","FRAMEBUFFER_STATUS:",status);           \
-            }                                                                         \
-            fprintf(stdout,"<%s:%d> ",::_tag_.c_str(),__LINE__);                      \
-            (void)fprintf(stdout,__VA_ARGS__);                                        \
-            fprintf(stdout,"\n");                                                     \
-            abort();                                                                  \
-         }                                                                            \
+      do { GLenum error_unique_niosfoinfsd = glGetError();                         \
+         if ( error_unique_niosfoinfsd != GL_NO_ERROR) {                           \
+            fprintf(stdout,"[%s 0x%04x] ","CORE ERROR:",error_unique_niosfoinfsd); \
+            if (error_unique_niosfoinfsd == 0x0506) { /*Framebuffer error*/        \
+               GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);           \
+               fprintf(stdout,"[%s 0x%04x] ","FRAMEBUFFER_STATUS:",status);        \
+            }                                                                      \
+            fprintf(stdout,"<%s:%d> ",::_tag_.c_str(),__LINE__);                   \
+            (void)fprintf(stdout,__VA_ARGS__);                                     \
+            fprintf(stdout,"\n");                                                  \
+            abort();                                                               \
+         }                                                                         \
       }while(0)
 
-#define FLOG_FATAL(...) FLOG_BASE(FCOLOR_GREY, FILLWAVE_FATAL,  FFATAL, __VA_ARGS__)
-#define FLOG_INFO(...) FLOG_BASE(FCOLOR_YELLOW, FILLWAVE_INFO,  FINFO, __VA_ARGS__)
-#define FLOG_DEBUG(...) FLOG_BASE(FCOLOR_GREEN, FILLWAVE_DEBUG,  FDEBUG, __VA_ARGS__)
-#define FLOG_USER(...) FLOG_BASE(FCOLOR_BLUE, FILLWAVE_USER,  FUSER, __VA_ARGS__)
-#define FLOG_ERROR(...) FLOG_BASE(FCOLOR_RED, FILLWAVE_ERROR,  FERROR, __VA_ARGS__)
-#define FLOG_WARNING(...) FLOG_BASE(FCOLOR_YELLOW, FILLWAVE_WARNING,  FWARNING, __VA_ARGS__)
+#define FLOG_FATAL(...) FLOG_BASE(FCOLOR_GREY, GPU_FATAL,  FFATAL, __VA_ARGS__)
+#define FLOG_INFO(...) FLOG_BASE(FCOLOR_YELLOW, GPU_INFO,  FINFO, __VA_ARGS__)
+#define FLOG_DEBUG(...) FLOG_BASE(FCOLOR_GREEN, GPU_DEBUG,  FDEBUG, __VA_ARGS__)
+#define FLOG_USER(...) FLOG_BASE(FCOLOR_BLUE, GPU_USER,  FUSER, __VA_ARGS__)
+#define FLOG_ERROR(...) FLOG_BASE(FCOLOR_RED, GPU_ERROR,  FERROR, __VA_ARGS__)
+#define FLOG_WARNING(...) FLOG_BASE(FCOLOR_YELLOW, GPU_WARNING,  FWARNING, __VA_ARGS__)
 
 #ifndef NDEBUG
 #endif /* NDEBUG */
