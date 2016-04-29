45b77997659086f8d54c2c6c0b7d52045187db59
filipwasil <fillwave@gmail.com>
2016-04-29 21:34:16 +0200

    #0 Files to auto format added in script

--- scripts/formatter.sh
+++ scripts/formatter.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-astyle --options=../doc/coding_standards/filllwave.astylerc --suffix=none --recursive "../src/*.cpp" "../inc/*.h*"
+astyle --options=../doc/coding_standards/filllwave.astylerc --suffix=none --recursive "../src/*.cpp" "../inc/*.h*" "../examples/linux/inc/*.h" "../examples/linux/src/*.cpp" "../examples/qt/*.cpp" "../examples/qt/*.h*"
