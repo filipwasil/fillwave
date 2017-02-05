ba53c516ce9d4f9ef8dd93ee2db5f925904cd75a
revcorey <rev@autograf.pl>
2017-02-05 17:01:09 +0100

    Remove unused buil folder.

--- editor/buil/CMakeFiles/assets_fillwave_qt_example.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "fillwave_qt_example_automoc.cpp"
-  "CMakeFiles/assets_fillwave_qt_example"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/assets_fillwave_qt_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
