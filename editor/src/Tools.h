e284b2eff649e6ffdec87cf20d4e622069cf1422
revcorey <rev@autograf.pl>
2017-02-11 12:54:26 +0100

    Special tools namespace created. In this file we add functions who can be use in difrent part of code, they are common.

---
+++ editor/src/Tools.h
@@ -0,0 +1,14 @@
+//
+// Created by radek on 11.02.17.
+//
+#ifndef FILLWAVE_QT_EXAMPLE_TOOLS_H
+#define FILLWAVE_QT_EXAMPLE_TOOLS_H
+#include <QFile>
+#include <QVector>
+#include <QTextStream>
+namespace tools
+{
+QVector<QString> readFileToStrings(QString pathToFile);
+}
+
+#endif //FILLWAVE_QT_EXAMPLE_TOOLS_H
