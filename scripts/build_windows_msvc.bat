baa9cd8854bf172b1cf5738df92b973c14b731a6
revcorey <rev@autograf.pl>
2017-05-09 21:04:51 +0200

    [#0] Fillwave compiled on windows as x64 instead of x86.

--- scripts/build_windows_msvc.bat
+++ scripts/build_windows_msvc.bat
@@ -2,6 +2,6 @@ cd ../..
 IF exist fillwave_windows_build_vs ( echo directory exists ) ELSE ( mkdir fillwave_windows_build_vs && echo fillwave_windows_build_vs created)
 mkdir fillwave_windows_build_vs
 cd fillwave_windows_build_vs
-cmake ../fillwave -G "Visual Studio 14 2015" -DASSIMP_BUILD_ASSIMP_TOOLS=NO -DBUILD_SHARED_LIBS:BOOL=false -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_TESTS=OFF -DGLFW_BUILD_TESTS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_DOCS=OFF
+cmake ../fillwave -G "Visual Studio 14 2015 Win64" -DASSIMP_BUILD_ASSIMP_TOOLS=NO -DBUILD_SHARED_LIBS:BOOL=false -DASSIMP_BUILD_TESTS=OFF -DASSIMP_BUILD_TESTS=OFF -DGLFW_BUILD_TESTS=OFF -DASSIMP_BUILD_ASSIMP_TOOLS=OFF -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_DOCS=OFF
 cmake --build .
 pause
