7fb5577fa0fbf9dbcfb4a0c8b13d7dc0c0019f80
filip <fillwave@gmail.com>
2017-05-20 20:20:47 +0200

    [#0] Coding style corrections

--- examples/editor/src/scene/ScensFactory.cpp
+++ examples/editor/src/scene/ScensFactory.cpp
@@ -20,56 +20,57 @@ namespace scene {
 ScensFactory::ScensFactory(int argc, char **argv)
     : mArgc(argc), mArgv(argv) {
   mScens["Hello World"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::TextScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::TextScene>(argc_, argv_);
   };
   mScens["Animation"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::AnimationScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::AnimationScene>(argc_, argv_);
+
   };
   mScens["Specular lights"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::FullModelScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::FullModelScene>(argc_, argv_);
+
   };
   mScens["Callbacks"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::CallbacksScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::CallbacksScene>(argc_, argv_);
+
   };
   mScens["Cursor"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::CursorScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::CursorScene>(argc_, argv_);
+
   };
   mScens["Dynamic texture"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::DynamicTextureScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::DynamicTextureScene>(argc_, argv_);
+
+  };
+  mScens["Picking"] = [](int argc_, char **argv_) {
+    return std::make_shared<scene::PickingScene>(argc_, argv_);
+
   };
   mScens["Effects"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::EffectsScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::EffectsScene>(argc_, argv_);
+
   };
   mScens["Hud"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::HudScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::HudScene>(argc_, argv_);
+
   };
   mScens["Ortographic projection"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::OrtpgraphicProjectionScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::OrtpgraphicProjectionScene>(argc_, argv_);
+
   };
   mScens["Particles"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::ParticlesScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::ParticlesScene>(argc_, argv_);
+
   };
   mScens["Post processing"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::PostProcessingScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::PostProcessingScene>(argc_, argv_);
+
   };
   mScens["Shadows"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::ShadowScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::ShadowScene>(argc_, argv_);
   };
   mScens["Skybox"] = [](int argc_, char **argv_) {
-    std::shared_ptr<scene::AScene> scen = std::make_shared<scene::SkyboxScene>(argc_, argv_);
-    return scen;
+    return std::make_shared<scene::SkyboxScene>(argc_, argv_);
   };
 }
 
