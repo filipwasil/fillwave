b60fa1d1bb3e42037f83a5159b9d4848f0d26c87
filip <fillwave@gmail.com>
2017-05-21 19:22:59 +0200

    [#0] Critical bug fixed. VAO init is not not related to VBO and IBO init in any case

--- src/models/Mesh.cpp
+++ src/models/Mesh.cpp
@@ -377,12 +377,12 @@ inline void Mesh::initVAO() {
 
   mVBO->bind();
   mVBO->attributesSetForVAO();
-  mVBO->setLoaded(GL_FALSE);
+  //mVBO->setLoaded(GL_FALSE);
   mVBO->send();
 
   if (mIBO) {
     mIBO->bind();
-    mIBO->setLoaded(GL_FALSE);
+    //mIBO->setLoaded(GL_FALSE);
     mIBO->send();
   }
 
