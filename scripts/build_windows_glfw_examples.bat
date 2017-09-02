1fcbfbb71dad9171763f4db008380bc2b924677d
revcorey <rev@autograf.pl>
2017-09-02 18:38:07 +0200

    [#0] Windows script for building glfw examples.

---
+++ scripts/build_windows_glfw_examples.bat
@@ -0,0 +1,9 @@
+cd ../../
+RD /S build_win_glfw_examples /Q
+MD build_win_glfw_examples
+cd build_win_glfw_examples
+cmake ../fillwave -DCMAKE_GENERATOR_PLATFORM=x64 -DFILLWAVE_BUILD_GLFW_EXAMPLES=ON
+cmake --build .
+xcopy ext\glew\DEBUG\glew64d.dll DEBUG\glew64d.dll*
+xcopy ext\assimp\code\Debug\assimp-vc140-mtd.dll DEBUG\assimp-vc140-mtd.dll*
+xcopy ..\fillwave\examples\data\* DEBUG\* /S
