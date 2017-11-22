4e165337c96df3476f2e6d9dd1a9ec7e2b7eff10
Filip Wasil <fillwave@gmail.com>
2017-11-22 21:27:38 +0100

    [#0] Scripts install update

--- scripts/setup_ubuntu_17.10.sh
+++ scripts/setup_ubuntu_17.10.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 sudo apt-get install libglew2.0 libglew-dev libglm-dev libassimp3v5 libassimp-dev libfreetype6 libfreetype6-dev
-
+sudo apt-get install doxygen valgrind vera++ cccc
 #libspdlog-dev package is broken in 17.10
 #/usr/include/spdlog/fmt/bundled/format.h: In member function ‘void fmt::internal::ArgFormatterBase<Impl,
 # Char>::visit_char(int)’:
