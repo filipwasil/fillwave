61d3f75e67fbd20f6911115b0d198d03407e55a2
revcorey <rev@autograf.pl>
2017-11-28 21:21:08 +0100

    [#0] Fix for failing scenefactory test.

--- examples/editor/tests/test/common/SceneEventFactoryTest.cpp
+++ examples/editor/tests/test/common/SceneEventFactoryTest.cpp
@@ -4,17 +4,17 @@ TEST_F(SceneEventFactoryTestFixture, testCreateDefualtKeyboardEvent
 ) {
 
 auto keboardEvent = sut.getDefaultKeyboardData(0);
-EXPECT_EQ(1, keboardEvent.action);
-EXPECT_EQ(0, keboardEvent.key);
-EXPECT_EQ(0, keboardEvent.mode);
-EXPECT_EQ(0, keboardEvent.scanCode);
+EXPECT_EQ(1, keboardEvent.mKey.action);
+EXPECT_EQ(0, keboardEvent.mKey.key);
+EXPECT_EQ(0, keboardEvent.mKey.mode);
+EXPECT_EQ(0, keboardEvent.mKey.scanCode);
 }
 
 TEST_F(SceneEventFactoryTestFixture, testCreateKeyboardEvent
 ) {
 auto keboardEvent = sut.getKeyboardData(0, 1, 2, 3);
-EXPECT_EQ(0, keboardEvent.action);
-EXPECT_EQ(1, keboardEvent.key);
-EXPECT_EQ(2, keboardEvent.mode);
-EXPECT_EQ(3, keboardEvent.scanCode);
+EXPECT_EQ(0, keboardEvent.mKey.action);
+EXPECT_EQ(1, keboardEvent.mKey.key);
+EXPECT_EQ(2, keboardEvent.mKey.mode);
+EXPECT_EQ(3, keboardEvent.mKey.scanCode);
 }
