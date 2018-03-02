fc0264b2a02e6d2a4aa4f586facc63423cf8d4e1
revcorey <rev@autograf.pl>
2018-03-02 20:17:18 +0100

    [#0] Removing interactive demos in qt.

--- scripts/build_linux_editor_rpm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd ../../
-rm -rf build_linux_deb_editor
-mkdir build_linux_deb_editor
-cd build_linux_deb_editor
-cmake ../fillwave -DFILLWAVE_BUILD_QT_EDITOR=ON -DFILLWAVE_BUILD_RPM=ON -DTESTSTATUS=OFF
-cmake --build examples/editor -- -j5
-cpack -c Relase
